768436755bab8df976a9e156297f21cb03d51423
Park Jaehyung - EiE Computer <explainpark101@gmail.com>
2022-12-29 20:34:21 +0900

    storage에서 가져오는 거 보완해야함

--- brower_action/index.js
+++ brower_action/index.js
@@ -11,18 +11,24 @@ const renderAll = async () => {
   const 학교 = document.querySelector("input[type='학교']");
   const 학년 = document.querySelector("input[type='학년']");
   const 연락처 = document.querySelector("input[type='연락처']");
-  await chrome.storage.local.get("이름",(result)=>{
-    이름.value = result.이름;
-  });
-  await chrome.storage.local.get("학교",(result)=>{
-    학교.value = result.학교;
-  });
-  await chrome.storage.local.get("학년",(result)=>{
-    학년.value = result.학년;
-  });
-  await chrome.storage.local.get("연락처",(result)=>{
-    연락처.value = result.연락처;
-  });
+  // chrome.storage.local.get("이름").then((result)=>{
+  //   이름.value = result.이름;
+  // });
+  // chrome.storage.local.get("학교").then((result)=>{
+  //   학교.value = result.학교;
+  // });
+  // chrome.storage.local.get("학년").then((result)=>{
+  //   학년.value = result.학년;
+  // });
+  // chrome.storage.local.get("연락처").then((result)=>{
+  //   연락처.value = result.연락처;
+  // });
+  Promise.all([hrome.storage.local.get("이름"), chrome.storage.local.get("학교"), chrome.storage.local.get("학년"), chrome.storage.local.get("연락처")])
+  .then(results=>{
+    results.forEach(result=>{
+      alert(result);
+    })
+  })
   
   [이름, 학교, 학년, 연락처].forEach(el=>{el.addEventListener("change", (e)=>{
     chrome.storage.local.set({"이름": 이름.value});
